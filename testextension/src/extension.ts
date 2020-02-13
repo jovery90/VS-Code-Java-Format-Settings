d70b98ba32daae87a3784f94b0521b3d050a8651
Jon Overy <jon.overy@trintech.com>
2020-02-13 10:48:25 -0600

    Fixed package.json, updated basic settings, and created ENABLE/DISABLE - separate commands

--- testextension/src/extension.ts
+++ testextension/src/extension.ts
@@ -5,8 +5,6 @@ import { format } from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-var activatedState = false;
-
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -16,29 +14,21 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.disable', () => {
 		// The code you place here will be executed every time your command is executed
+		vscode.window.showInformationMessage('DEACTIVATED auto-formatting');
 
-		// Display a message box to the user
-		if (activatedState === false) {
-			vscode.window.showInformationMessage('DEACTIVATED auto-formatting');
-			const test = vscode.workspace.getConfiguration('testExtension');
-			test.inspect("formatOnSave")?.globalValue;
+		vscode.workspace.getConfiguration().update("editor.formatOnSave", false, true)
+		vscode.workspace.getConfiguration().update("editor.formatOnPaste", false, true)
+		vscode.workspace.getConfiguration().update("editor.formatOnType", false, true)
+	});
 
-			vscode.workspace.getConfiguration().update("editor.formatOnSave", false, true) 
-			vscode.workspace.getConfiguration().update("editor.formatOnPaste", false, true)
-			vscode.workspace.getConfiguration().update("editor.formatOnType", false, true)
+	let disposable2 = vscode.commands.registerCommand('extension.enable', () => {
 
-			activatedState = true;
-		}
-		else {
-			vscode.workspace.getConfiguration().update("editor.formatOnSave", true, true)
-			vscode.workspace.getConfiguration().update("editor.formatOnPaste", true, true)
-			vscode.workspace.getConfiguration().update("editor.formatOnType", true, true)
-			vscode.window.showInformationMessage('ACTIVATED auto-formatting');
-			activatedState = false
-		}
-
+		vscode.workspace.getConfiguration().update("editor.formatOnSave", true, true)
+		vscode.workspace.getConfiguration().update("editor.formatOnPaste", true, true)
+		vscode.workspace.getConfiguration().update("editor.formatOnType", true, true)
+		vscode.window.showInformationMessage('ACTIVATED auto-formatting');
 
 	});
 
